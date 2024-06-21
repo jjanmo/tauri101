3d1349b1363b32bb753fd72742a87990dad89b64
jjanmo <jjanmo07@hanmail.net>
2024-06-22 01:52:32 +0900

    fix: 절대경로 설정 추가

--- vanilla/vite.config.ts
+++ vanilla/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -13,7 +18,7 @@ export default defineConfig(async () => ({
     strictPort: true,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+      ignored: ['**/src-tauri/**'],
     },
   },
 }));
